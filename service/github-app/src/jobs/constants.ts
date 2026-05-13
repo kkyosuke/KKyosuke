0352e281891217c955ca8fc9f9b3d99fddcf2d68
kyosuke <kyosuke.k.94@gmail.com>
2026-05-13 22:45:39 +0900

    chore: enable fallthrough for GitHub App KV asset bindings in wrangler configuration

--- service/github-app/src/jobs/constants.ts
+++ service/github-app/src/jobs/constants.ts
@@ -1,4 +1,4 @@
 // @ts-ignore
-import pkg from "../../../package.json" with { type: "json" };
+import pkg from "../../package.json" with { type: "json" };
 
 export const IN_PROGRESS_PLACEHOLDER_COMMENT = `> [!NOTE]\n> 🔍 **Review in Progress**\n> 現在コードのレビュー中です。完了まで少々お待ちください！\n> version: ${pkg.version}`;
